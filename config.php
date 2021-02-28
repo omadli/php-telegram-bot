4260d07890fec38f33166a4f46d65fc556438e3b
omadli <76625693+omadli@users.noreply.github.com>
2021-02-28 10:30:21 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $token = "1234567890:AAAAAAAAAAAAAAAAAAAAAAAAA"; //Bot tokeni
+    $token = "1234567890:AAAAAAAAAAAAAAAAAAAAAAAAA"; //Bot tokeni md5() orqali shifrlab qo'ying. Tokenni o'zinisaqlash xavfsiz emas!
     $ADMIN = 123456789; //glavni admin id si
     $admins = [123456789, 1234567890]; // hamma adminlar id lari joylashgan array
     $logsch =  -100123456789;// Loglar kanali id si
