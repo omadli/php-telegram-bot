f5cae0d32682c646c4aca40e8ce67bf45bb750a9
omadli <76625693+omadli@users.noreply.github.com>
2021-02-28 10:21:55 +0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,21 +1,38 @@
 <?php
 // Agar Updatelarni webhook orqali olmoqchi bo'lsangiz bu faylni webhook ga ulang
-if (file_exists("bot.php")){
-    require_once "bot.php";
-} else {
-    echo "Please include bot.php file!";
-    exit();
-}
-include "config.php";
-$bot = new Bot($token, $ADMIN, $logsch, $admins); //bot obyekti
+require_once(__DIR__."/config.php");
+$ws = $_GET['ws'] ?? null;
+// Webhook qilishda tokenni GET metodi orqali ws paramatrida ko'rsatish kerak.
+// config.php faylida Tokenni o'zini emas shifrlangan token hashini saqlash maqsadga muvofiq
 
-$update = $bot->getWebhookData();
-if (!empty($update)){
-    try{
-        include __DIR__ . '/main.php';
-    } catch(Exception $e){
-        $e = json_encode($e, JSON_PRETTY_PRINT);
-        $bot->sm($bot->logsch, "#Exception\n<pre>{$e}</pre>", "html");
-    }
+if(!empty($ws) && md5($ws) == $secret){
+  if (file_exists("bot.php")){
+      require_once "bot.php";
+  } else {
+      echo "Please include bot.php file!";
+      exit();
+  }
+  $token = $ws;
+  $bot = new Bot($token, $ADMIN, $logsch, $admins); //bot obyekti
+  echo "Bot ish faoliyatida";
+  $update = $bot->getWebhookData();
+  if (!empty($update)){
+      try{
+          include __DIR__ . '/main.php';
+      } catch(Exception $e){
+          $e = json_encode($e->getMessage(), JSON_PRETTY_PRINT);
+          $bot->sm($bot->logsch ?? $bot->ADMIN, "#Exception\n<pre>{$e}</pre>", "html");
+      }
+      if((isset($update['message']) && in_array($update['message']['from']['id'], $bot->admins)) || (isset($update['callback_query']) && in_array($update['callback_query']['from']['id'], $bot->admins))){
+        try{
+            include __DIR__ . '/admin.php';
+        } catch(Exception $e){
+            $e = json_encode($e->getMessage(), JSON_PRETTY_PRINT);
+            $bot->sm($bot->logsch ?? $bot->ADMIN, "#Exception\n<pre>{$e}</pre>", "html");
+        }  
+      }
+  }
+} else {
+  echo("<b>Warning! You don't permission danied!</b>");
 }
  ?>
